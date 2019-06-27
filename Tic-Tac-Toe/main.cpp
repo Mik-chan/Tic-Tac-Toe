aca4d7a86f5fcad4f5b82ccee3b25a265c12cd08
wtlgo <georgeendo0@gmail.com>
2019-06-27 18:20:23 +0300

    Initial Commit

---
+++ Tic-Tac-Toe/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tic-Tac-Toe
+//
+//  Created by George Endo on 27.06.2019.
+//  Copyright © 2019 George Endo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
